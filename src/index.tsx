0baf6093e2d4b6a24292f51762323134504cfc00
PeterSkriba <peterskriba@hotmail.com>
2021-03-09 13:48:03 +0100

    API documentation added.

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import App from './App'
+
+import 'swagger-ui-react/swagger-ui.css'
+
+render(<App />, document.getElementById('root'))
